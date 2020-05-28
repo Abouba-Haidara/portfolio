f4a533f696677a802585ef3d72ce08b7d2cf9f26
Yemini Coder <31940004+Abouba-Haidara@users.noreply.github.com>
2020-05-28 11:38:01 +0000

    update

--- index.php
+++ index.php
@@ -288,7 +288,7 @@
                                     <div class="item animate" data-animate="fadeInUp">
                                         <div class="portfolio-item">
                                             <div class="thumb box">
-                                                <img src="images/item-2.jpg" alt="">
+                                                <img src="images/item-2.png" alt="">
                                             </div>
                                             <div class="thumb-inner animate" data-animate="fadeInUp">
                                                 <h4>Fariz IoT</h4>
