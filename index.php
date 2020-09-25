1a8f18d3cc029a846418baf1ee276c6032f0e703
Yemini Coder <31940004+Abouba-Haidara@users.noreply.github.com>
2020-09-25 12:48:02 +0000

    Update index.php

--- index.php
+++ index.php
@@ -144,7 +144,7 @@
                                         </div>
                                     </div>
                                     <div class="col-md-6">
-                                        <br /><br />
+                                        <br /><br /><br /> <br />
                                         <figure class="about-img animate" data-animate="fadeInUp"><img
                                                 src="images/Profile.jpg" class="rounded" alt="absent"></figure>
                                     </div>
