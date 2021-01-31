4cb5b3c5bfcba7ea0f6c2d2bf0f8fc312a5982ea
Yemini Coder <31940004+Abouba-Haidara@users.noreply.github.com>
2021-01-31 20:38:20 +0000

    Update index.php

--- index.php
+++ index.php
@@ -405,7 +405,7 @@
                                         <div class="item animate" data-animate="fadeInUp">
                                             <div class="testimonial-item">
                                                 <div class="client-row">
-                                                    <img src="images/profile-" class="rounded-circle" alt="profile 2">
+                                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGWU8M739rK4xasc_VM2SdcMLDxDCKtY93jw&usqp=CAU" class="rounded-circle" alt="profile 2">
                                                 </div>
                                                 <div class="testimonial-content">
                                                     <h4>Soon...</h4>
